2fd6c06907e56118c6b0988a52e0d3377eaac1d5
ajoybhunia <ajoybabu143@gmail.com>
2025-09-22 09:39:26 +0530

    Printing the reverse form of a given number

--- reverse_of_a_number.js
+++ reverse_of_a_number.js
@@ -1,7 +1,7 @@
-const givenNumber = 121;
+const givenNumber = -121;
 let copyOfNumber = givenNumber;
 let reverse = 0;
-while(copyOfNumber > 0){
+while(copyOfNumber !== 0){
   let lastDigit = copyOfNumber % 10;
   reverse = reverse * 10 + lastDigit;
   copyOfNumber = (copyOfNumber - lastDigit) / 10; 
