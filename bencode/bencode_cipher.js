5626452bf03a1f635d65771758e9914498a212e5
ajoybhunia <ajoybabu143@gmail.com>
2025-10-31 08:45:33 +0530

    Update bencode_cipher.js

--- bencode/bencode_cipher.js
+++ bencode/bencode_cipher.js
@@ -28,39 +28,46 @@ function areDeepEqual(array1, array2) {
   return array1 === array2;
 }
 
+function decodeList(bencodedString, decoded) {
+  let index = 1;
+
+  while (index < bencodedString.length) {
+    if (bencodedString[index] === 'l') {
+      const subResult = decode(bencodedString.slice(index));
+      decoded.push(subResult);
+      return decoded;
+    }
+    if (`${parseInt(bencodedString[index])}` !== 'NaN') {
+      const newBencodedString = bencodedString.slice(index, bencodedString.length - 1);
+      const lengthOfString = parseInt(newBencodedString.slice(0, newBencodedString.indexOf(':')));
+      const bencodedListElements = newBencodedString.slice(0, lengthOfString + `${lengthOfString}`.length + 1);
+      const jump = lengthOfString + `${lengthOfString}`.length;
+      decoded.push(decode(bencodedListElements.slice(0)));
+      index += jump + 1;
+    } else if (bencodedString[index] === 'i') {
+      const newBencodedString = bencodedString.slice(index, bencodedString.length - 1);
+      const bencodedListElements = newBencodedString.slice(0, newBencodedString.indexOf('e') + 1);
+      const jump = bencodedListElements.indexOf('e');
+      decoded.push(decode(bencodedListElements.slice(0, jump)));
+      index += jump + 1;
+    } else {
+      index++;
+    }
+  }
+}
+
 function decode(bencodedString) {
   if (bencodedString[0] === 'l') {
     const decoded = [];
-    let index = 1;
+    decodeList(bencodedString, decoded);
 
-    while (index < bencodedString.length) {
-      if (bencodedString[index] === 'l') {
-        const subResult = decode(bencodedString.slice(index));
-        decoded.push(subResult);
-        return decoded;
-      }
-      if (`${parseInt(bencodedString[index])}` !== 'NaN') {
-        let newBencodedString = bencodedString.slice(index, bencodedString.length - 1);
-        const lengthOfString = parseInt(newBencodedString.slice(0, newBencodedString.indexOf(':')));
-        let bencodedListElements = newBencodedString.slice(0, lengthOfString + `${lengthOfString}`.length + 1);
-        const jump = lengthOfString + `${lengthOfString}`.length;
-        decoded.push(decode(bencodedListElements.slice(0)));
-        index += jump + 1;
-      } else if (bencodedString[index] === 'i') {
-        let newBencodedString = bencodedString.slice(index, bencodedString.length - 1);
-        let bencodedListElements = newBencodedString.slice(0, newBencodedString.indexOf('e') + 1);
-        const jump = bencodedListElements.indexOf('e');
-        decoded.push(decode(bencodedListElements.slice(0, jump)));
-        index += jump + 1;
-      } else {
-        index++;
-      }
-    }
     return decoded;
   }
+
   if (bencodedString[0] === 'i') {
     return parseInt(bencodedString.slice(1));
   }
+  
   return bencodedString.slice(bencodedString.indexOf(':') + 1);
 }
 
@@ -114,6 +121,7 @@ function testAllEncodeTestCases() {
   test(encode(['one', 'two']), 'l3:one3:twoe', 'List of Strings');
   test(encode([0, 'one', 'two', 3]), 'li0e3:one3:twoi3ee', 'List of Both String and Numbers');
   test(encode([1, 'two', ['three', 4]]), 'li1e3:twol5:threei4eee', 'Nested List');
+  test(encode([1, 'two', ['three', 4], 1]), 'li1e3:twol5:threei4eei1ee', 'Nested List');
   test(encode([]), 'le', 'Empty List');
 }
 
@@ -137,7 +145,7 @@ function testAllDecodeTestCases() {
 }
 
 function main() {
-  testAllEncodeTestCases();
+  // testAllEncodeTestCases();
   testAllDecodeTestCases();
 }
 
