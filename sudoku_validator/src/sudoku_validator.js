4ffd0aa7fea0f898a4da20521b906c30c45b6e24
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2025-12-14 22:03:49 +0530

    happy happy happy!

--- sudoku_validator/src/sudoku_validator.js
+++ sudoku_validator/src/sudoku_validator.js
@@ -1,11 +1,13 @@
 import { chunk } from "@std/collections";
 
+const regex = /^[1-9]$/;
+
 export const isValid = (row) => {
   const seen = [];
 
   for (const cell of row) {
     if (cell !== "") {
-      if (!/^[1-9]$/.test(cell)) return false;
+      if (!regex.test(cell)) return false;
       if (seen.includes(cell)) return false;
 
       seen.push(cell);
@@ -15,18 +17,18 @@ export const isValid = (row) => {
   return true;
 };
 
-const generateCols = (board) =>
+const transposeMatrix = (board) =>
   board[0].map((_, colIndex) => board.map((row) => row[colIndex]));
 
-const generateGrids = (board) =>
+const gridMatrix = (board) =>
   chunk(board, 3)
     .flatMap((gridRows) =>
       [0, 3, 6].map((col) => gridRows.flatMap((r) => r.slice(col, col + 3)))
     );
 
 export const isValidBoard = (board) => {
-  const cols = generateCols(board);
-  const grids = generateGrids(board);
+  const cols = transposeMatrix(board);
+  const grids = gridMatrix(board);
 
   return [...board, ...cols, ...grids].every((each) => isValid(each));
 };
