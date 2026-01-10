7e2f5a99d393989995471dc7801873fee973f8e6
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2026-01-10 23:08:25 +0530

    Hid config.js

--- projects/dictionary_cli/src/dictionary.js
+++ projects/dictionary_cli/src/dictionary.js
@@ -1,4 +1,4 @@
-import { API_URL } from "./config.js";
+import { API_URL } from "../config.js";
 import * as cache from "./cache.js";
 
 export const getDefinition = async (
--- projects/dictionary_cli/src/formatter.js
+++ projects/dictionary_cli/src/formatter.js
@@ -2,6 +2,7 @@ const MAX = 2;
 
 export const formatDefinition = (json) => {
   const data = json[0];
+
   let output = `\n\t${data.word.toLowerCase()}\n`;
 
   for (const meaning of data.meanings) {
